785899cda6cb03aea4e96115f4517610ba6326d3
migaldottir <none399@outlook.com>
2023-02-09 22:50:00 +0100

    defined CardHolder constructor

--- Program.cs
+++ Program.cs
@@ -9,4 +9,13 @@
     String lastName;
     double balance;
 
+    public CardHolder(String cardNum, int pin, String firstName, String lastName double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+
+    }
 }
