9a197fffafeb060b36ecc79a438eebd20441a094
migaldottir <none399@outlook.com>
2023-02-10 14:49:50 +0100

    defined a function responsible for money deposit, corrected previous code

--- Program.cs
+++ Program.cs
@@ -19,18 +19,13 @@
 
     }
 
-    public String getCardNum() {
-        return cardNum; }
+    public String getCardNum() { return cardNum; }
 
-    public int getPin() { return pin;
-    }
+    public int getPin() { return pin; }
 
-    public String getFirstName() { 
-        return firstName; }
-    public String getLastName() { 
-        return lastName; }
-    public double getBalance() { 
-        return balance; }
+    public String getFirstName() {  return firstName; }
+    public String getLastName() { return lastName; }
+    public double getBalance() { return balance; }
 
 
     public void setNum(String newNum) { cardNum = newNum }
@@ -43,5 +38,22 @@
 
     public static void Main(Sting[] args)
     { 
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+        void deposit (cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to deposit?")
+                double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit);
+            Console.WriteLine("Your new balance is: " + currentUser.getBalance());
+        }
+
+        void withdraw (CardHolder currentUser)
     }
 }
